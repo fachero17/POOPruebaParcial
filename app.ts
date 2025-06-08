9ce7a0c4a83276474e824edf0d4416a4b4ecea51
Cesar Andres Garcia Barcia <119273281+fachero17@users.noreply.github.com>
2025-06-08 18:21:29 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
-
+import {Hotel} from './Hotel';
+import {Habitacion} from './Habitacion';
+import {Cliente} from './cliente';
+import {Factura} from './factura';
+import {Pago} from './pago';
+import {Reserva} from './reserva';
