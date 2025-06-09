7752b6d95157a8168f2f728d20c36464c52953af
Ronnieplus18 <ronito.ostaiza2004@gmail.com>
2025-06-09 15:24:43 -0500

    Update factura.ts

--- factura.ts
+++ factura.ts
@@ -1,17 +1,8 @@
-import { Cliente } from './Cliente';
-import { Reserva } from './Reserva';
-import { Pago } from './Pago';
-import { ServicioAdicional } from './ServicioAdicional';
-
 export class Factura {
   constructor(
     private numero: number,
     private fechaEmision: Date,
     private total: number,
-    private cliente: Cliente,
-    private reserva: Reserva,
-    private pago: Pago,
-    private servicios: ServicioAdicional[]
   ) {}
 
   public generarPDF(): void {}
