a0baa38eb95cb1ac50dc55a43a9601d985d88c1b
Cesar Andres Garcia Barcia <119273281+fachero17@users.noreply.github.com>
2025-06-09 15:42:45 -0500

    Update Hotel.ts

--- Hotel.ts
+++ Hotel.ts
@@ -1,14 +1,12 @@
 import { Habitacion } from './Habitacion';
 
 export class Hotel {
-  private habitaciones: Habitacion[] = [];
   constructor(
     public nombre: string,
     public direccion: string,
     public telefono: string
     ) {}
 
- mostrarHabitacionesDisponibles(): void {
-    console.log(`${this.nombre} tiene ${this.habitacionesDisponibles} habitaciones disponibles.`);
-  }  gestionarServicios(){}
+ mostrarHabitacionesDisponibles(){}  
+  gestionarServicios(){}
 }
