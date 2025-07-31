eb0ccdddfa1af5010e864d0865e8db0e3fc49031
Cesar Andres Garcia Barcia <119273281+fachero17@users.noreply.github.com>
2025-07-30 21:03:54 -0500

    Delete app.ts

--- app.ts
+++
@@ -1,6 +0,0 @@
-import {Hotel} from './Hotel';
-import {Habitacion} from './Habitacion';
-import {Cliente} from './cliente';
-import {Factura} from './factura';
-import {Pago} from './pago';
-import {Reserva} from './reserva';
